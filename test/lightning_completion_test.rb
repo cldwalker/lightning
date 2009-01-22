6add7935da09dea6c60f73f3d89d157ee4332131
Gabriel Horner <gabriel.horner@gmail.com>
2009-01-22 15:26:17 -0500

    os-independent required test paths

--- test/lightning_completion_test.rb
+++ test/lightning_completion_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/test_helper'
+require File.join(File.dirname(__FILE__), 'test_helper')
 
 class LightningCompletionTest < Test::Unit::TestCase
   context "Completion" do
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ require 'test/unit'
 require 'context' #gem install jeremymcanally-context -s http://gems.github.com
 require 'stump' #gem install jeremymcanally-stump -s http://gems.github.com
 require 'pending' #gem install jeremymcanally-pending -s http://gems.github.com
-$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'lightning'
 
 class Test::Unit::TestCase
