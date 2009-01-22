720003a4b1987aca6618d5a1d8e04099a1b6a0e9
Gabriel Horner <gabriel.horner@gmail.com>
2009-01-22 18:39:46 -0500

    renamed PathMap to CompletionMap

--- test/lightning_bolt_test.rb
+++ test/lightning_bolt_test.rb
@@ -3,25 +3,25 @@ require File.join(File.dirname(__FILE__), 'test_helper')
 class LightningBoltTest < Test::Unit::TestCase
   context "Bolt" do
     before(:each) do
-      @path_map = {'path1'=>'/dir/path1','path2'=>'/dir/path2'}
+      @completion_map = {'path1'=>'/dir/path1','path2'=>'/dir/path2'}
       @bolt = Lightning::Bolt.new('blah')
-      @bolt.path_map.map = @path_map
+      @bolt.completion_map.map = @completion_map
     end
     
     test "fetches correct completions" do
-      assert_equal @bolt.completions, @path_map.keys
+      assert_equal @bolt.completions, @completion_map.keys
     end
 
     test "resolves completion" do
-      assert_equal @path_map['path1'], @bolt.resolve_completion('path1')
+      assert_equal @completion_map['path1'], @bolt.resolve_completion('path1')
     end
 
     test "resolves completion with test flag" do
-      assert_equal @path_map['path1'], @bolt.resolve_completion('-test path1')
+      assert_equal @completion_map['path1'], @bolt.resolve_completion('-test path1')
     end
 
-    test "creates path_map only once" do
-      assert_equal @bolt.path_map.object_id, @bolt.path_map.object_id
+    test "creates completion_map only once" do
+      assert_equal @bolt.completion_map.object_id, @bolt.completion_map.object_id
     end
   end
 end
--- test/lightning_completion_map_test.rb
+++ test/lightning_completion_map_test.rb
@@ -1,30 +1,30 @@
 require File.join(File.dirname(__FILE__), 'test_helper')
 
-class LightningPathMapTest < Test::Unit::TestCase
+class LightningCompletionMapTest < Test::Unit::TestCase
 
-  context "PathMap" do    
+  context "CompletionMap" do    
     def create_map(path_hash)
       Dir.stub!(:glob) { path_hash.values }
-      @path_map = Lightning::PathMap.new('blah') 
+      @completion_map = Lightning::CompletionMap.new('blah') 
     end
     
     test "creates basic map" do
       expected_map = {"path1"=>"/dir1/path1", "path2"=>"/dir1/path2"}
       create_map(expected_map)
-      assert_equal expected_map, @path_map.map
+      assert_equal expected_map, @completion_map.map
     end
     
     test "ignores paths from Lightning.ignore_paths" do
       Lightning.stub!(:ignore_paths, :return=>['path1'])
       expected_map = {"path1"=>"/dir1/path1", "path2"=>"/dir1/path2"}
       create_map(expected_map)
-      assert_equal expected_map.slice('path2'), @path_map.map
+      assert_equal expected_map.slice('path2'), @completion_map.map
     end
     
     test "creates map with duplicates" do
       expected_map = {"path1//dir3"=>"/dir3/path1", "path2"=>"/dir1/path2", "path1//dir1"=>"/dir1/path1", "path1//dir2"=>"/dir2/path1"}
       create_map(expected_map)
-      assert_equal expected_map, @path_map.map
+      assert_equal expected_map, @completion_map.map
     end    
   end
 end
