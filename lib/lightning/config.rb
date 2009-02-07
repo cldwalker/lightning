1ba183a71864ff91e0b7b1aca89a306c6ff7eb6a
Gabriel Horner <gabriel.horner@gmail.com>
2009-02-06 21:56:57 -0500

    added basic alias, path and lcommand commands for binary

--- lib/lightning/config.rb
+++ lib/lightning/config.rb
@@ -1,5 +1,6 @@
 module Lightning::Config
   attr_accessor :config_file
+  attr_accessor :slim_config
   def load_config
     @config = setup_config
   end
@@ -14,22 +15,87 @@ module Lightning::Config
   
   def setup_config
     hash = read_config_file
-    configure_commands_and_paths(hash)
+    configure_commands_and_paths(hash) unless @slim_config
     hash
+  end
+  
+  def config_file
+    @config_file ||= (File.exists?('lightning.yml') ? 'lightning.yml' : File.expand_path(File.join("~",".lightning.yml")))
+  end
+  
+  def save_config(config=Lightning.config)
+    File.open(config_file, "w") { |f| f.puts config.to_yaml }
   end
   
   def read_config_file(file=nil)
     default_config = {:shell=>'bash', :generated_file=>File.expand_path(File.join('~', '.lightning_completions')), 
       :complete_regex=>true}
-    @config_file = file || @config_file || (File.exists?('lightning.yml') ? 'lightning.yml' : File.expand_path(File.join("~",".lightning.yml")))
-    hash = YAML::load(File.new(@config_file))
+    @config_file = file if file
+    hash = YAML::load_file(config_file)
     default_config.merge(hash.symbolize_keys)
   end
   
-  def config_command(name)
-    config[:commands].find {|e| e['name'] == name} || {}
+  def config_command(name, hardcheck=false)
+    command = config[:commands].find {|e| e['name'] == name} || {}
+    if hardcheck && command.empty?
+      puts "Command '#{name}' not found"
+      nil
+    else
+      command
+    end
   end
   
+  def config_command_paths(name)
+    if command = config_command(name, true)
+      if command['paths'].is_a?(String)
+         Lightning.config[:paths][command['paths']]
+      else
+        command['paths']
+      end
+    else
+      []
+    end
+  end
+  
+  def add_command_alias(command_name, path_alias, path)
+    Lightning.slim_config = true
+    if (command = config_command(command_name, true))
+      path = File.expand_path(path)
+      command['aliases'][path_alias] = path
+      save_config
+      puts "Path alias '#{path_alias}' added"
+    end
+  end
+  
+  def delete_command_alias(command_name, path_alias)
+    Lightning.slim_config = true
+    if (command = config_command(command_name, true))
+      command['aliases'].delete(path_alias)
+      save_config
+      puts "Path alias '#{path_alias}' deleted"
+    end
+  end
+  
+  def add_path(command_name, path)
+    Lightning.slim_config = true
+    if (command = config_command(command_name, true))
+      path = File.expand_path(path)
+      config_command_paths(command_name) << path
+      save_config
+      puts "Path '#{path}' added"
+    end
+  end
+  
+  def delete_path(command_name, path)
+    Lightning.slim_config = true
+    if (command = config_command(command_name, true))
+      path = File.expand_path(path)
+      config_command_paths(command_name).delete(path)
+      save_config
+      puts "Path '#{path}' deleted"
+    end
+  end
+    
   def commands_to_bolt_key(map_to_command, new_command)
     "#{map_to_command}-#{new_command}"
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'test/unit'
 require 'context' #gem install jeremymcanally-context -s http://gems.github.com
 require 'stump' #gem install jeremymcanally-stump -s http://gems.github.com
-require 'pending' #gem install jeremymcanally-pending -s http://gems.github.com
+#require 'pending' #gem install jeremymcanally-pending -s http://gems.github.com
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'lightning'
 #set up valid global config file
