d573a71c37b2044fa305631863dc2c3f40340b0a
Gabriel Horner <gabriel.horner@gmail.com>
2009-12-04 12:08:47 -0500

    more config tweaks

--- lib/lightning/config.rb
+++ lib/lightning/config.rb
@@ -1,42 +1,35 @@
 class Lightning  
   class Config < ::Hash
+    DEFAULT = {'shell'=>'bash', 'complete_regex'=>true, 'paths'=>{},
+      'generated_file'=>File.expand_path(File.join('~', '.lightning_completions'))}
+
     attr_accessor :config_file
-    def initialize(hash=nil)
-      hash ||= read_config_file
-      hash[:paths] ||= {}
+    def initialize(hash=read_config_file)
+      hash = DEFAULT.merge hash
       super
       replace(hash)
       self.replace(self.symbolize_keys)
     end
     
+    def read_config_file
+      YAML::load_file(config_file)
+    end
+
     def config_file
-      @config_file ||= (File.exists?('lightning.yml') ? 'lightning.yml' : File.expand_path(File.join("~",".lightning.yml")))
+      @config_file ||= (File.exists?('lightning.yml') ? 'lightning.yml' :
+        File.expand_path(File.join("~",".lightning.yml")))
     end
 
-    def read_config_file(file=nil)
-      default_config = {'shell'=>'bash', 'generated_file'=>File.expand_path(File.join('~', '.lightning_completions')),
-        'complete_regex'=>true}
-      @config_file = file if file
-      hash = YAML::load_file(config_file)
-      default_config.merge(hash)
-    end
-
-    def to_hash
-      hash = Hash.new
-      hash.replace(self)
-    end
-    
     def save
-      File.open(config_file, "w") { |f| f.puts self.to_hash.to_yaml }
+      File.open(config_file, "w") { |f| f.puts self.to_yaml }
     end
 
     #from Rails' ActiveSupport
     def symbolize_keys
       inject({}) do |options, (key, value)|
-        options[(key.to_sym rescue key) || key] = value
+        options[key.to_sym] = value
         options
       end
     end
-    
   end
 end
