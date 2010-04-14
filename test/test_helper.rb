adb13c7e972e04949faec3bc092ca79ed80aa0c9
Gabriel Horner <gabriel.horner@gmail.com>
2010-04-14 17:33:58 -0400

    tweaked bacon adapter for rr and moved to separate file

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 require 'bacon'
 require 'rr'
 require File.dirname(__FILE__)+'/bacon_extensions'
+require File.dirname(__FILE__)+'/bacon_rr'
 require 'lightning'
 #set up valid global config file
 Lightning::Config.config_file = File.join(File.dirname(__FILE__), 'lightning.yml')
@@ -39,24 +40,6 @@ end
 class Bacon::Context
   include Helpers
   include BaconExtensions
-
-  # RR adapter
-  def self.silent?
-    @silent ||= class << Bacon; self; end.ancestors.include?(Bacon::TestUnitOutput)
-  end
-  include RR::Adapters::RRMethods
-  RR.trim_backtrace = true
-  alias_method :old_it, :it
-	def it(description)
-    old_it(description) do
-      # Add at least one requirement to ensure mock-only tests don't fail
-      Bacon::Counter[:requirements] += 1
-      yield
-      Bacon::Counter[:requirements] -= 1 if RR.double_injections.size.zero?
-      RR.verify unless self.class.silent?
-      RR.reset
-    end
-	end
 
   def xtest(*args); end
   def xcontext(*args); end
