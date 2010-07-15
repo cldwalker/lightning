249d703f00bcaae8133007ca6b27b74ae590fc19
Gabriel Horner <gabriel.horner@gmail.com>
2010-07-15 17:43:38 -0400

    comment out nonexistent method for rr gem

--- test/bacon_rr.rb
+++ test/bacon_rr.rb
@@ -8,7 +8,7 @@ class Bacon::Context
         # Add at least one requirement to ensure mock-only tests don't fail
         Bacon::Counter[:requirements] += 1
         yield
-        Bacon::Counter[:requirements] -= 1 if RR.double_injections.size.zero?
+        #Bacon::Counter[:requirements] -= 1 if RR.double_injections.size.zero?
         RR.verify
       rescue RR::Errors::RRError=>e
         raise Bacon::Error.new(:failed, e.message)
